a13788827c1d7ba01049fe0ff97c826d5ac60320
tetsudaux <nicolas.osnaya@gmail.com>
2023-05-27 16:51:21 -0600

    Se agrego la aleatoridad del enemigo

--- ejercicio2/js/mokepon.js
+++ ejercicio2/js/mokepon.js
@@ -25,7 +25,25 @@ function seleccionarMascotaJugador () {
     else {
         alert("Por favor selecciona tu mascota")
     }
+
+    seleccionarMascotaEnemigo()
 }
 
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if(ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratiqgueya'
+    }
+}
 
 window.addEventListener("load", iniciarJuego)
